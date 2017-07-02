60b191d70caae3905b8b6d969a0a0dae0244c45c
EC2 Default User <ec2-user@ip-172-31-26-115.us-east-2.compute.internal>
2017-07-02 10:49:51 +0000

    challenge 02 - pebble docker image

---
+++ challenge02-docker/pebble_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: Vivek KC
+# Description: Pebble docker image
+mkdir $HOME/gforge
+cd $HOME/gforge/
+git clone https://github.com/kcvivek/v-challenge.git
+v-challenge/challenge02-docker
+docker build -t pebble_kc:latest .
+docker run -it --rm -d -p 80:8080 pebble_kc
+
+docker login
+docker tag pebble_kc kcvivek/pebble:pebble_kc
+sudo docker push kcvivek/pebble:pebble_kc
+
+cat <<-EOM
+Docker Repo: https://hub.docker.com/r/kcvivek/pebble
+
+To run on a new box do:
+	docker pull kcvivek/pebble
+	docker run -it --rm -d -p 80:8080 pebble_kc
+
+	docker ps
+
+Browse: http://localhost/pebble
+
+EOM
